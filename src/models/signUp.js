0e019cd6f40358d4cefb3fed084c5a724fd473d2
Arthur Victoriano <arthurvicto@hotmail.com>
2022-07-18 03:50:11 -0300

    Refactor: refatorando a função de criar carteira para uma model específica

--- src/models/signUp.js
+++ src/models/signUp.js
@@ -6,12 +6,6 @@ const createClient = async (name, email, password) => {
   return insertId;
 };
 
-const createWallet = async (clientId) => {
-  const [result] = await connection.execute(`INSERT INTO XPInc.wallets (idClient) VALUES (?)`, [clientId]);
-  return result;
-};
-
 module.exports = {
   createClient,
-  createWallet,
 };
