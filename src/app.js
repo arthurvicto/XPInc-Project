7cab95afad2161c62cdb940a2cb34a0faf28a156
Arthur Victoriano <arthurvicto@hotmail.com>
2022-07-18 01:33:27 -0300

    feat: Cria rota get, "/ativos/:id", que retorna o asset(ativo) pelo ID único

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const assets = require('./controllers/assets');
+const express = require("express");
+const assets = require("./controllers/assets");
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('XPinc-API-Project');
+app.get("/", (req, res) => {
+  res.send("XPinc-API-Project");
 });
 
-app.get('/ativos', assets.allAssetsFromServices);
+app.get("/ativos", assets.allAssetsFromServices);
+app.get("/ativos/:id", assets.assetsById);
 
 module.exports = app;
--- src/services/assets.js
+++ src/services/assets.js
@@ -1,10 +1,16 @@
-const getAssets = require('../models/assets');
+const getAssets = require("../models/assets");
 
 const allAssets = async () => {
   const assets = await getAssets.getAllAssets();
   return assets;
 };
 
+const assetsById = async (id) => {
+  const assets = await getAssets.getAssetById(id);
+  return assets;
+};
+
 module.exports = {
   allAssets,
+  assetsById,
 };
