c9d134d24d537e0b9764cef5604868da27a11f2b
Arthur Victoriano <arthurvicto@hotmail.com>
2022-07-23 14:37:05 -0300

    feat: implementando testes no models, rota em que verifica saldo do cliente

---
+++ src/test/unit/models/clientWallet.test.js
@@ -0,0 +1,39 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { clientWalletById } = require('../../../models/wallet');
+const connection = require('../../../models/connections');
+
+describe('Visualiza o saldo do cliente com a chamada clientWalletById', () => {
+  describe('quando é realizado com sucesso', () => {
+     before(async () => {
+      const execute =[[
+        {
+          idWallet: 1,
+          idClient: 1,
+          balance: '0',
+        }]];
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+    after(async () => { connection.execute.restore(); });
+
+    it ('Verifica se o retorno da função é um objeto', async () => {
+      const response = await clientWalletById(1);
+      expect(response).to.be.an('object');
+    
+    });
+     it ('Verifica se o objeto retornado contém as informações necessárias: ( idWallet, idClient, balance)', async () => {
+      const response = await clientWalletById(1);
+      expect(response.idWallet).to.be.an('number');
+      expect(response.idClient).to.be.an('number');
+      expect(response.balance).to.be.an('string');
+    });
+     it('Verifica se o objeto não retornou vazio', async () => {
+        const response = await clientWalletById(1);
+        expect(response).to.not.be.empty;;
+      });
+      it ('Verifica se o objeto tem as chaves: (idWallet, idClient e balance)', async () => {
+        const response = await clientWalletById(1);
+        expect(response).to.have.all.keys('idWallet', 'idClient', 'balance');
+        });
+  });
+});
