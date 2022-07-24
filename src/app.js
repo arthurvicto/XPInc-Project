33ffb1bd3fa97740278f35b2098f0962c9ffb91b
Arthur Victoriano <arthurvicto@hotmail.com>
2022-07-24 19:30:34 -0300

    refactor: nomes dos endpoins

--- src/app.js
+++ src/app.js
@@ -1,24 +1,31 @@
 const express = require('express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 const clientsRouter = require('./routes/clients');
 const newWalletRouter = require('./routes/createWallet');
 const assetsRouter = require('./routes/assets');
 const signUpRouter = require('./routes/signUp');
 const walletRouter = require('./routes/wallet');
 const investmentRouter = require('./routes/investment');
+const { swaggerConfig } = require('./docs/swagger.config');
 require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
 
+const swaggerDoc = swaggerJSDoc(swaggerConfig);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+
 app.get('/', (req, res) => {
   res.send('XPinc-API-Project');
 });
 
 app.use('/ativos', assetsRouter);
-app.use('/signup', signUpRouter);
-app.use('/clients', clientsRouter);
-app.use('/createwallet', newWalletRouter);
+app.use('/cadastro', signUpRouter);
+app.use('/cliente', clientsRouter);
+app.use('/criarCarteira', newWalletRouter);
 app.use('/conta', walletRouter);
 app.use('/investimento', investmentRouter);
 
