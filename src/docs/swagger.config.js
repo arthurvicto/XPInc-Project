64e7fff8dbc7c60a65fa35b2fbf110892601d5bc
Arthur Victoriano <arthurvicto@hotmail.com>
2022-07-24 19:32:00 -0300

    fet: adicionando rota docs, para documentação swagger

---
+++ src/docs/swagger.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const { PORT } = process.env;
+
+const swaggerConfig = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'XPInc-Project API',
+      description: 'API documentation for XPInc-Project',
+      version: '1.0',
+    },
+    servers: [{
+      url: `http://localhost:${PORT}`,
+      description: 'DB- heroku',
+    }],
+  },
+  apis: ['./src/index.js', './src/routes/assets.js', './src/routes/clients.js',
+    './src/routes/createWallet.js', './src/routes/investment.js', './src/routes/signUp.js',
+    './src/routes/wallet.js'],
+};
+
+module.exports = {
+  swaggerConfig,
+};
--- src/routes/assets.js
+++ src/routes/assets.js
@@ -5,11 +5,81 @@ const assetsOnBank = require('../middlewares/assetsOnBank');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *  tags:
+ *     name: ativos
+ *     description: Endpoints de ativos do Banco XPInc
+ */
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      ativos:
+ *        type: object
+ *        required:
+ *          - idAsset
+ *        properties:
+ *           idAsset:
+ *              type: integer
+ *              description: ID do ativo
+ *           name:
+ *              type: string
+ *              description: Nome do ativo
+ *           value:
+ *              type: string
+ *              description: Preço do ativo
+ *           qtde:
+ *              type: integer
+ *              description: Quantidade do ativo
+ *        example:
+ *          idAsset: 4
+ *          name: "AZUL4"
+ *          qtde: 9976
+ *          value: "350,00"
+ */
+
+/**
+ * @swagger
+ * /ativos:
+ *      get:
+ *        tags: [ativos]
+ *        description: Retorna todos os ativos do Banco XPInc
+ *        responses:
+ *          200:
+ *            content:
+ *               application/json:
+ *                   schema:
+ *                      type: array
+ *                      items:
+ *                         $ref: '#/components/schemas/ativos'
+ */
 router.get(
   '/',
   assetsOnBank,
   assets.allAssetsFromServices,
 );
+
+/**
+ * @swagger
+ * /ativos/{idAsset}:
+ *                get:
+ *                  tags: [ativos]
+ *                  description: Retorna um ativo por idAsset
+ *                  parameters:
+ *                    - in: path
+ *                      name: idAsset
+ *                      type: integer
+ *                      required: true
+ *                  responses:
+ *                    200:
+ *                      content:
+ *                        application/json:
+ *                          schema:
+ *                           type: object
+ *                           $ref: '#/components/schemas/ativos'
+ */
 router.get(
   '/:idAsset',
   assetsOnBank,
