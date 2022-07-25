32ec8e15669fed0f5a9e47096866ec19a470d5c4
Arthur Victoriano <arthurvicto@hotmail.com>
2022-07-24 21:43:52 -0300

    feat: documentação swagger rota /investimento

--- src/routes/investment.js
+++ src/routes/investment.js
@@ -11,6 +11,62 @@ const authTokenTransactions = require('../middlewares/authTokenTransactions');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ *  tags:
+ *     name: investimento
+ *     description: Endpoints de transação de ações do clientes
+ */
+
+/**
+ * @swagger
+ *  components:
+ *    schemas:
+ *      investimento:
+ *        type: object
+ *        required:
+ *          - idWallet
+ *          - idAsset
+ *          - qtde
+ *        properties:
+ *           idWallet:
+ *              type: integer
+ *              description: cod carteira do cliente
+ *           idAsset:
+ *              type: integer
+ *              description: cod ação
+ *           qtde:
+ *              type: integer
+ *              description: quantidade de ações
+ *        example:
+ *          idWallet: 24
+ *          idAsset: 4
+ *          qtde: 5
+ */
+
+/**
+ * @swagger
+ *  /investimento/comprar:
+ *          post:
+ *            tags: [investimento]
+ *            description: Realiza compra de ações
+ *            security:
+ *             - bearerAuth: []
+ *            requestBody:
+ *                required: true
+ *                content:
+ *                 application/json:
+ *                   schema:
+ *                    type: object
+ *            responses:
+ *              200:
+ *                content:
+ *                  application/json:
+ *                    schema:
+ *                     type: object
+ *                     $ref: '#/components/schemas/investimento'
+ */
+
 router.post(
   '/comprar',
   tokenAuth,
@@ -22,6 +78,30 @@ router.post(
   clientWalletValidator,
   investment.buyAssets,
 );
+
+/**
+ * @swagger
+ *  /investimento/vender:
+ *          post:
+ *            tags: [investimento]
+ *            description: Realiza venda de ações
+ *            security:
+ *             - bearerAuth: []
+ *            requestBody:
+ *                required: true
+ *                content:
+ *                 application/json:
+ *                   schema:
+ *                    type: object
+ *            responses:
+ *              200:
+ *                content:
+ *                  application/json:
+ *                    schema:
+ *                     type: object
+ *                     $ref: '#/components/schemas/investimento'
+ */
+
 router.post(
   '/vender',
   tokenAuth,
